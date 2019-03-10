3c514ec42a170e62c7edfdd6fe52102f05e11e68
private-yusuke <townstock@ksf.biglobe.ne.jp>
2019-03-10 13:32:14 +0900

    abc120(a,b,c,d),abc121(a,b,c,d),wupc2019(a)

---
+++ AtCoder/abc121/d_.d
@@ -0,0 +1,91 @@
+void main() {
+	auto ip = readAs!(ulong[]), A = ip[0], B = ip[1];
+	ulong res;
+	if(B==A) { writeln(A); return; }
+	
+	ulong k = 60;
+	while(k != 0) {
+		scope(exit) k--;
+		ulong mask = 1UL << k;
+		ulong flag;
+		debug writefln("k = %d, res = %d, mask = %d", k, res, mask);
+		if(mask > B) continue;
+		ulong i = 1UL << (k-1);
+		
+		if(A >= mask) {
+			ulong s = (A - i) % mask;
+			debug writefln("A >= mask, i = %d, s = %d", i, s);
+			if(s < i) {
+				flag ^= 1;
+			}
+		}
+		ulong s = (B - i) % mask;
+		debug writefln("B, i = %d, s = %d", i, s);
+		if(s < i) {
+			flag ^= 1;
+		}
+		writefln("flag = %d, flag << k = %d", flag, flag << k);
+		res += flag << k;
+	}
+	ulong flag;
+	if(B % 2) flag = 1;
+	if(A % 2) flag ^= 1;
+	if((B-A+1) % 2) flag ^= 1;
+	res += flag;
+	res.writeln;
+}
+
+// ===================================
+
+import std.stdio;
+import std.string;
+import std.functional;
+import std.algorithm;
+import std.range;
+import std.traits;
+import std.math;
+import std.container;
+import std.bigint;
+import std.numeric;
+import std.conv;
+import std.typecons;
+import std.uni;
+import std.ascii;
+import std.bitmanip;
+import core.bitop;
+
+T readAs(T)() if (isBasicType!T) {
+	return readln.chomp.to!T;
+}
+T readAs(T)() if (isArray!T) {
+	return readln.split.to!T;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (!isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		res[i] = readAs!(T[]);
+	}
+	return res;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		auto s = rs;
+		foreach(j; 0..width) res[i][j] = s[j].to!T;
+	}
+	return res;
+}
+
+int ri() {
+	return readAs!int;
+}
+
+double rd() {
+	return readAs!double;
+}
+
+string rs() {
+	return readln.chomp;
+}
