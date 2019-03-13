d45846c4522b8f4aecdf530b426b77850bfc7527
private-yusuke <townstock@ksf.biglobe.ne.jp>
2019-03-13 13:19:01 +0900

    agc008(c)

---
+++ AtCoder/agc008/c.d
@@ -0,0 +1,64 @@
+void main() {
+	auto ip = readAs!(long[]), I = ip[0], O = ip[1], T = ip[2], J = ip[3], L = ip[4], S = ip[5], Z = ip[6];
+	long res = 2*(I/2+J/2+L/2) + O;
+	if(I > 0 && J > 0 && L > 0) {
+		I--; J--; L--;
+		res = max(res, 2*(I/2+J/2+L/2) + 3 + O);
+	}
+	res.writeln;
+}
+
+// ===================================
+
+import std.stdio;
+import std.string;
+import std.functional;
+import std.algorithm;
+import std.range;
+import std.traits;
+import std.math;
+import std.container;
+import std.bigint;
+import std.numeric;
+import std.conv;
+import std.typecons;
+import std.uni;
+import std.ascii;
+import std.bitmanip;
+import core.bitop;
+
+T readAs(T)() if (isBasicType!T) {
+	return readln.chomp.to!T;
+}
+T readAs(T)() if (isArray!T) {
+	return readln.split.to!T;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (!isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		res[i] = readAs!(T[]);
+	}
+	return res;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		auto s = rs;
+		foreach(j; 0..width) res[i][j] = s[j].to!T;
+	}
+	return res;
+}
+
+int ri() {
+	return readAs!int;
+}
+
+double rd() {
+	return readAs!double;
+}
+
+string rs() {
+	return readln.chomp;
+}
