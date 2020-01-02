dc5cab2b3cd081b75e30ab54039cecd15dac20e2
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-01-02 19:44:01 +0900

    template(fft),abc149(e)

--- AtCoder/abc149/e.d
+++ AtCoder/abc149/e.d
@@ -1,3 +1,20 @@
+void main() {
+	auto ip = readAs!(long[]), N = ip[0], M = ip[1];
+	auto A = readAs!(long[]);
+	auto arr = new long[](A.reduce!max+1);
+	A.each!(v => arr[v]++);
+	auto arr2 = FFT!double.Convolve(arr, arr);
+	ulong k = arr2.length-1;
+	ulong res;
+	while(M > 0 && k > 0) {
+		auto use = min(M, arr2[k]);
+		M -= use;
+		res += k * use;
+		k--;
+	}
+	res.writeln;
+}
+
 import std.complex;
 // Complex!T は isFloatingPoint!T を要求する。
 class FFT(T = double) if(isFloatingPoint!T) {
@@ -33,4 +50,59 @@ class FFT(T = double) if(isFloatingPoint!T) {
 		}
 		return f;
 	}
+}
+
+// ===================================
+
+import std.stdio;
+import std.string;
+import std.functional;
+import std.algorithm;
+import std.range;
+import std.traits;
+import std.math;
+import std.container;
+import std.bigint;
+import std.numeric;
+import std.conv;
+import std.typecons;
+import std.uni;
+import std.ascii;
+import std.bitmanip;
+import core.bitop;
+
+T readAs(T)() if (isBasicType!T) {
+	return readln.chomp.to!T;
+}
+T readAs(T)() if (isArray!T) {
+	return readln.split.to!T;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (!isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		res[i] = readAs!(T[]);
+	}
+	return res;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		auto s = rs;
+		foreach(j; 0..width) res[i][j] = s[j].to!T;
+	}
+	return res;
+}
+
+int ri() {
+	return readAs!int;
+}
+
+double rd() {
+	return readAs!double;
+}
+
+string rs() {
+	return readln.chomp;
 }
