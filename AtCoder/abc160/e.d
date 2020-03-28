5486795af0f6e4f9fc4869285ebed9703141a0f5
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-03-28 23:10:39 +0900

    abc160(a,b,c,d,e)

---
+++ AtCoder/abc160/e.d
@@ -0,0 +1,66 @@
+void main() {
+	auto ip = readAs!(long[]), X = ip[0], Y = ip[1], A = ip[2], B = ip[3], C = ip[4];
+	auto p = readAs!(long[]);
+	auto q = readAs!(long[]);
+	auto r = readAs!(long[]);
+	p.sort!"a > b"(); q.sort!"a > b"(); r.sort!"a > b"();
+	p = p[0..X]; q = q[0..Y];
+	long[] arr = r ~ p ~ q;
+	arr.sort!"a > b"();
+	arr[0..X+Y].sum.writeln;
+}
+
+// ===================================
+
+import std.stdio;
+import std.string;
+import std.functional;
+import std.algorithm;
+import std.range;
+import std.traits;
+import std.math;
+import std.container;
+import std.bigint;
+import std.numeric;
+import std.conv;
+import std.typecons;
+import std.uni;
+import std.ascii;
+import std.bitmanip;
+import core.bitop;
+
+T readAs(T)() if (isBasicType!T) {
+	return readln.chomp.to!T;
+}
+T readAs(T)() if (isArray!T) {
+	return readln.split.to!T;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (!isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		res[i] = readAs!(T[]);
+	}
+	return res;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		auto s = rs;
+		foreach(j; 0..width) res[i][j] = s[j].to!T;
+	}
+	return res;
+}
+
+int ri() {
+	return readAs!int;
+}
+
+double rd() {
+	return readAs!double;
+}
+
+string rs() {
+	return readln.chomp;
+}
