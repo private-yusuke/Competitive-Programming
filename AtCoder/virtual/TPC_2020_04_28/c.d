1ccc0e1018d357a00a1b1fd08cf6a84a6f7d4271
public_yusuke <30387586+private-yusuke@users.noreply.github.com>
2020-05-03 18:26:00 +0900

    TPC_2020_04_28(a,b,c)

---
+++ AtCoder/virtual/TPC_2020_04_28/c.d
@@ -0,0 +1,95 @@
+void main() {
+	auto N = ri;
+	auto A = readAs!(int[]);
+	ulong k;
+	int tmp;
+	foreach(i, v; A) if(tmp < v) {
+		k = i;
+		tmp = v;
+	}
+	tmp = 0;
+	while(true) {
+		ulong nextk;
+		debug k.writeln;
+		debug A.writeln;
+		if(A[k] == 0) {
+			writeln("YES");
+			return;
+		}
+		debug "====".writeln;
+		foreach(i; 1..N+1) {
+			auto m = (k+i)%N;
+
+			if(A[m] >= i) {
+				A[m] -= i;
+				if(tmp < A[m]) {
+					debug writefln("nextk = %d", m);
+					nextk = m;
+					tmp = A[m];
+				}
+			} else {
+				debug writefln("k: %d, i: %d, m: %d, A[m]: %d", k, i, m, A[m]);
+				debug A.writeln;
+				writeln("NO");
+				return;
+			}
+		}
+		tmp = 0;
+		k = nextk;
+	}
+}
+
+// ===================================
+
+import std.stdio;
+import std.string;
+import std.functional;
+import std.algorithm;
+import std.range;
+import std.traits;
+import std.math;
+import std.container;
+import std.bigint;
+import std.numeric;
+import std.conv;
+import std.typecons;
+import std.uni;
+import std.ascii;
+import std.bitmanip;
+import core.bitop;
+
+T readAs(T)() if (isBasicType!T) {
+	return readln.chomp.to!T;
+}
+T readAs(T)() if (isArray!T) {
+	return readln.split.to!T;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (!isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		res[i] = readAs!(T[]);
+	}
+	return res;
+}
+
+T[][] readMatrix(T)(uint height, uint width) if (isSomeChar!T) {
+	auto res = new T[][](height, width);
+	foreach(i; 0..height) {
+		auto s = rs;
+		foreach(j; 0..width) res[i][j] = s[j].to!T;
+	}
+	return res;
+}
+
+int ri() {
+	return readAs!int;
+}
+
+double rd() {
+	return readAs!double;
+}
+
+string rs() {
+	return readln.chomp;
+}
